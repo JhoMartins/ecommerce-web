d821c98af9f2918b7237fb526e9f5e4a24a04def
Jhonatan Martins <jhonatanbmartins@gmail.com>
2020-12-04 22:01:38 -0300

    clearing index tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 
+import Head from 'next/head';
+
 import '../styles/globals.css'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps}/>
+  return(
+    <>
+      <Head>
+        <title>OneBitGames</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width"></meta>
+      </Head>
+      <Component {...pageProps}/>
+    </>
+  )
 }
 
-export default MyApp
+export default MyApp;
