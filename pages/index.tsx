a66899a2605f828fb3bf06e0cd80932a722e91e4
Jhonatan Martins <jhonatanbmartins@gmail.com>
2020-12-06 12:08:31 -0300

    creates login form

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import MainComponent from '../components/shared/MainComponent';
+import { useRouter } from 'next/router';
+
 
 const Home: React.FC = () => {
+  const router = useRouter();
+
   return (
-    <h1>Hello World</h1>
+    <MainComponent>
+      <button onClick={() => router.push('/Auth/Login')}>Login</button>
+    </MainComponent>
   )
 }
 
