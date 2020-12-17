3a9606fd6a32d8c6fc10b476f114732319bb5a63
Jhonatan Martins <jhonatanbmartins@gmail.com>
2020-12-16 21:05:48 -0300

    add dinamic behavior to login form

--- store/modules/auth/reducer.ts
+++ store/modules/auth/reducer.ts
@@ -3,16 +3,16 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import User from '../../../dtos/User';
 
 const authSlice = createSlice({
-  name: 'auth',
-  initialState: { loggedUser: null as User } ,
-  reducers: {
-    setLoggedUser(state, action: PayloadAction<User>) {
-        state.loggedUser = action.payload; 
-    },
-    clearLoggedUser(state) {
-        state.loggedUser = null;
-    },
-  }
+    name: 'auth',
+    initialState: { loggedUser: null as User } ,
+    reducers: {
+      setLoggedUser(state, action: PayloadAction<User>) {
+          state.loggedUser = action.payload; 
+      },
+      clearLoggedUser(state) {
+          state.loggedUser = null;
+      },
+    }
 })
 
 export const { setLoggedUser, clearLoggedUser } = authSlice.actions;
